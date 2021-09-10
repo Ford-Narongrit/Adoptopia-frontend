dbe926948d89983026571b0d75b2ee8f3c2f0e6a
ford-narongrit <narongrit.th@ku.th>
2021-09-10 16:42:54 +0700

    update README.md

--- src/views/User/id.vue
+++ src/views/User/id.vue
@@ -5,12 +5,15 @@
       <div class="space-y-3">
         <div class="my-text-super text-white">{{ user.name }}</div>
         <div class="my-text-content text-white">{{ user.username }}</div>
-        <div class="my-text-content text-white">this is description</div>
+        <div class="my-text-content text-white">{{user.description}}</div>
       </div>
     </div>
     <div class="flex justify-center space-x-5">
       <button class="bg-shark-600 rounded-lg py-3 px-10 text-white my-text-content hover:bg-shark-400 my-block-focus">ADOT</button>
       <button class="bg-blue-600 rounded-lg py-3 px-10 text-white my-text-content hover:bg-blue-400 my-block-focus">POST</button>
+    </div>
+    <div>
+      TODO
     </div>
   </div>
 </template>
