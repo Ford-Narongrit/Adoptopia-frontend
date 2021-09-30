57e6b311e70fb98f9417e03c3be015ca441b0226
Arreeya <arreeya.su@ku.th>
2021-09-30 22:41:55 +0700

    add vue coverflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   purge: [],
@@ -48,5 +49,38 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      const buttons = {
+        '.btn-rounded': {
+          background: '#1E63E9',
+          borderRadius: '20px',
+          flex: 'none',
+          width: '6em',
+          height: '2.2em',
+          fontSize: '1rem',
+
+          '&:hover': {
+            color: '#1E63E9',
+            background: '#ffffff'
+          }
+        },
+
+        '.btn-square': {
+          background: '#1E63E9',
+          borderRadius: '5px',
+          flex: 'none',
+          width: '6em',
+          height: '2.2em',
+          fontSize: '1rem',
+
+          '&:hover': {
+            color: '#1E63E9',
+            background: '#ffffff'
+          }
+        }
+      }
+      addComponents(buttons);
+    })
+  ],
 };
