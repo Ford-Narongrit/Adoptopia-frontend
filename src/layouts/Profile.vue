6f0b60d2315af559d72a3a02ffb1a3156c186f59
ford-narongrit <narongrit.th@ku.th>
2021-10-02 00:38:02 +0700

    add myAdopt page

--- src/layouts/Profile.vue
+++ src/layouts/Profile.vue
@@ -80,9 +80,7 @@ export default {
   name: "ProfileLayout",
   data() {
     return {
-      user: {},
-      bgImage:
-        "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+      user: {}
     };
   },
   mounted() {
--- src/views/user/Adopt.vue
+++ src/views/user/Adopt.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-      this is Adopt in Profile
+    {{ adopt }}
   </div>
 </template>
 
 <script>
+import UserStore from "@/store/User";
 export default {
-
-}
+  data() {
+    return {
+      adopt: [],
+    };
+  },
+  mounted() {
+    this.fetch();
+  },
+  methods: {
+    async fetch() {
+      try {
+        let res = await UserStore.dispatch("getMe");
+        console.log(res);
+        this.adopt = res.data.user;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getImagePath(image) {
+      return process.env.VUE_APP_APIURL + image;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
