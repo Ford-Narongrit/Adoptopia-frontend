848a611f8f8862e8abb87b561d79ac11829fdc5c
ford-narongrit <narongrit.th@ku.th>
2021-10-14 18:11:35 +0700

    finish feature loadingUser

--- src/views/user/Adopt_id.vue
+++ src/views/user/Adopt_id.vue
@@ -9,12 +9,30 @@
     </div>
 
     <div v-else class="lg:flex block h-screen">
-      <slide-image :images="adopt.adopt_image" class="w-full h-3/4 lg:h-full" />
+      <div v-if="loading" class="w-full flex items-center justify-center">
+        <div class="w-96 h-96 bg-gray-200 rounded-lg animate-pulse"></div>
+      </div>
+      <slide-image
+        v-if="!loading"
+        :images="adopt.adopt_image"
+        class="w-full h-3/4 lg:h-full"
+      />
       <div class="bg-shark-800 lg:w-96 py-10 px-3 space-y-3">
-        <div class="text-white my-text-title text-center">
+        <div
+          v-if="loading"
+          class="bg-white h-10 rounded-lg animate-pulse"
+        ></div>
+        <div class="text-white my-text-title text-center" v-if="!loading">
           {{ adopt.name }}
         </div>
-        <div>
+        <div v-if="loading" class="grid grid-cols-4 gap-1 animate-pulse">
+          <div class="h-5 col-span-2 rounded-lg bg-white"></div>
+          <div class="h-5 col-span-1 rounded-lg bg-white"></div>
+          <div class="h-5 col-span-1 rounded-lg bg-white"></div>
+          <div class="h-5 col-span-1 rounded-lg bg-white"></div>
+          <div class="h-5 col-span-2 rounded-lg bg-white"></div>
+        </div>
+        <div v-if="!loading">
           <span
             v-for="category in adopt.category"
             :key="category.id"
@@ -26,13 +44,36 @@
           </span>
         </div>
         <div class="text-white my-text-subtitle">Agreement</div>
-        <div class="text-white my-text-content bg-gray-600 rounded-lg p-2">
+        <div v-if="loading" class="bg-gray-600 rounded-lg p-2">
+          <div class="grid grid-cols-4 gap-1 animate-pulse">
+            <div class="h-6 col-span-2 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-1 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-1 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-1 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-2 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-4 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-2 rounded-lg bg-white"></div>
+            <div class="h-6 col-span-2 rounded-lg bg-white"></div>
+          </div>
+        </div>
+        <div
+          v-if="!loading"
+          class="text-white my-text-content bg-gray-600 rounded-lg p-2"
+        >
           {{ adopt.agreement }}
         </div>
-        <div class="text-white my-text-base">
+        <div
+          v-if="loading"
+          class="bg-white h-10 rounded-lg animate-pulse"
+        ></div>
+        <div
+          v-if="loading"
+          class="bg-white h-10 rounded-lg animate-pulse"
+        ></div>
+        <div v-if="!loading" class="text-white my-text-base">
           Created at: {{ getFormatTime(adopt.created_at) }}
         </div>
-        <div class="text-white my-text-base">
+        <div v-if="!loading" class="text-white my-text-base">
           Receive at: {{ getFormatTime(adopt.updated_at) }}
         </div>
       </div>
@@ -50,6 +91,7 @@ export default {
     return {
       adopt: {},
       errors: null,
+      loading: true,
     };
   },
   mounted() {
@@ -63,6 +105,7 @@ export default {
           this.$route.params.id
         );
         this.adopt = AdoptStore.getters.adopt;
+        this.loading = false;
       } catch (error) {
         this.errors = error.response;
         console.error(error.response);
